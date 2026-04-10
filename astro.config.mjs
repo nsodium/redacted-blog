c0563db92166e69226d2ff592bbb52959a8d8ed5
Nahleen Scovell <nscovell@pop-os.localdomain>
2026-04-10 14:20:12 -0400

    make img show up

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://nsodium.github.io/redacted-blog",
+	base: '/redacted-blog/',
 	output: 'static',
 });
