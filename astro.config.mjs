4d4ff9c1618fced0c1cafe42d34737e348964d3b
Nahleen Scovell <nscovell@pop-os.ht.home>
2026-04-21 18:58:15 -0400

    Switch to custom domain hydropolis.ca

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://nsodium.github.io/redacted-blog",
-	base: '/redacted-blog/',
+	site: "https://hydropolis.ca",
 	output: 'static',
 });
